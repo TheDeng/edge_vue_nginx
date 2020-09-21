b9f145433cd6ad7282aec23779b1dd51acc04a66
TheDeng <dengjiale1996@163.com>
2020-09-21 11:50:07 +0800

    upload

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import BaiduMap from 'vue-baidu-map'
+
+Vue.config.productionTip = false
+Vue.use(BaiduMap,{
+  ak:'YqIZWCd4x5gpYX4bWt8GqgPbacvrpIgi',
+})
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
